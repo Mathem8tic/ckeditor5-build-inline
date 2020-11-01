99d90b0237fa1c877b8ba1829f049540e72c4d3e
Matthew Harris <matt@harris.pro>
2020-10-31 21:43:21 -0400

    remove custom eleemtn

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -29,7 +29,7 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
-import CustomElementPlugin from 'ckeditor5-custom-element/src/customelement';
+// import CustomElementPlugin from 'ckeditor5-custom-element/src/customelement';
 import MediaLibrary from './media-library/medialibrary';
 import customIcon from './custom.svg';
 
@@ -60,8 +60,8 @@ InlineEditor.builtinPlugins = [
 	TableToolbar,
 	TextTransformation,
 	MediaLibrary,
-	ImageResize,
-	CustomElementPlugin
+	ImageResize
+	// CustomElementPlugin
 ];
 
 // Editor configuration.
